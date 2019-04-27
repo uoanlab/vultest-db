f6f4f8e83df9a077bb56dd27c6e753fc856e6724
redsloop <redsloop.ko@gmail.com>
2019-04-27 19:01:46 +0900

    [fix] リファレンスの追加, .gitignoreの修正

--- setup.rb
+++ setup.rb
@@ -1,3 +1,26 @@
+=begin
+Copyright [2019] [Kohei Akasaka]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Copyright (c) 2004, Jamis Buck (jamis@jamisbuck.org)
+Relased under the BSD license
+https://github.com/sparklemotion/sqlite3-ruby/blob/master/LICENSE
+
+This software includes the work that is distributed in the Apache License 2.0
+=end
+
+
 require 'bundler/setup'
 
 require 'fileutils'
