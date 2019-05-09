acbee74f0ee9d18bba5975c344cf2a3b870f8995
redsloop <redsloop.ko@gmail.com>
2019-05-09 20:02:06 +0900

    [fix] ライセンスの修正

--- setup.rb
+++ setup.rb
@@ -12,12 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-Copyright (c) 2004, Jamis Buck (jamis@jamisbuck.org)
-Relased under the BSD license
-https://github.com/sparklemotion/sqlite3-ruby/blob/master/LICENSE
-
-This software includes the work that is distributed in the Apache License 2.0
 =end
 
 
